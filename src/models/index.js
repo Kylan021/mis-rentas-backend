322d9955cc88915b8e1cb4f37880802d413c3c66
Andres <andrestorrespaternina04@gmail.com>
2025-12-08 19:14:47 -0500

    feat(db): definir modelos y relaciones de Sequelize

---
+++ src/models/index.js
@@ -0,0 +1,31 @@
+
+const Sequelize = require("sequelize");
+const sequelize = require("src/config/database");
+
+const User = require('src/models/user')(sequelize, Sequelize.DataTypes);
+const Property = require("src/models/property")(sequelize, Sequelize.DataTypes);
+const Booking = require("src/models/booking")(sequelize, Sequelize.DataTypes);
+const BlockedDate = require("src/models/blockedDate")(sequelize, Sequelize.DataTypes);
+
+User.hasMany(Property, { foreignKey: 'owner_id', as: 'properties' });
+Property.belongsTo(User, { foreignKey: 'owner_id', as: 'owner' });
+
+User.hasMany(Booking, { foreignKey: 'traveler_id', as: 'bookings' });
+Booking.belongsTo(User, { foreignKey: 'traveler_id', as: 'traveler' });
+
+Property.hasMany(Booking, { foreignKey: 'property_id', as: 'bookings' });
+Booking.belongsTo(Property, { foreignKey: 'property_id', as: 'property' });
+
+Property.hasMany(BlockedDate, { foreignKey: 'property_id', as: 'blockedDates' });
+BlockedDate.belongsTo(Property, { foreignKey: 'property_id', as: 'property' });
+
+const db = {
+    sequelize,
+    Sequelize,
+    User,
+    Property,
+    Booking,
+    BlockedDate
+};
+
+module.exports = db;
